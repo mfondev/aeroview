2627af28f80fb956c2f5ddeb3c3b6fca02093544
Gideon Atauba <ataubagideon@gmail.com>
2025-02-19 20:40:46 -0800

    added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import MainHeader from "@/components/header/mainHeader";
 import Faq from "@/components/faq/faq";
-import Footer from "@/components/footer/footer";
+import Footer from "@/components/navigation/footer";
 import FlightPage from "@/components/flights/mainFlight";
 import Testimonial from "@/components/testimonial";
 
 export default function Home() {
+  
   return (
     <>
       <MainHeader />
--- src/components/flights/mainFlight.tsx
+++ src/components/flights/mainFlight.tsx
@@ -4,8 +4,7 @@ import AvailableFlights from "./availableFlights";
 import SearchFlights from "./searchFlights";
 import Contact from "./contact";
 import FlightsLink from "./flightsLink";
-import FlightSummary from "./flightSummary";
-import Cta from "./cta";
+
 
 export default function FlightPage() {
   return (
@@ -13,9 +12,9 @@ export default function FlightPage() {
       <FlightHeader />
       <AvailableFlights />
       <div className="bg-[#3661eb] w-full">
-        <div className=" max-w-[800px] pl-8 py-18 text-white text-left">
+        <div className=" max-w-[60%]  pl-16 py-20 text-white text-left">
           <h3 className="text-[48px]">
-            Airlines save a number of <span>$7,084</span> discounted seats for
+            Airlines save a number of <br /> <span>$7,084</span> discounted seats for
             every flight <span></span> called "consolidator" fares. <br />
             Now they're yours. <span></span>
           </h3>
@@ -27,8 +26,8 @@ export default function FlightPage() {
         </div>
       </div>
       <main className="bg-[#dee4eb] ">
-        <section className=" pt-12">
-          <article className="container mx-auto">
+        <section className="">
+          <article className="container mx-auto py-16">
             <div>
               <h3 className="text-center text-[32px] max-w-[430px] mx-auto">
                 <span className="text-[#4360ba] text-[24px] font-bold">So easy</span>,
@@ -41,19 +40,17 @@ export default function FlightPage() {
                 Discounted, "insider" fares. At your fingertips.
               </p>
             </div>
-            <main className="py-4 flex flex-row items-center justify-between">
+            <main className="py-8 flex flex-row items-center justify-center  gap-6">
               <SearchFlights />
               <Contact />
               <FlightsLink />
             </main>
-            {/* <button className="flex items-center justify-between bg-[#3661eb] text-white py-2 px-4 rounded-[5px] text-center mx-auto">
+            <button className="flex items-center justify-between bg-[#3661eb] text-white py-2 px-4 rounded-[5px] text-center mx-auto">
               Search Now
-            </button> */}
+            </button>
           </article>
         </section>
-
       </main>
-      
     </>
   );
 }
